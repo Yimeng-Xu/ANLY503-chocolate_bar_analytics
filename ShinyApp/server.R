73b5eabc8c8864e135d458d6ea58348a27de0317
Yimeng-Xu <35519677+Yimeng-Xu@users.noreply.github.com>
2018-12-18 15:26:44 +0800

    ShinyApp

---
+++ ShinyApp/server.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+cocoa <- read.csv('data/flavors_of_cocao_clean-united states.csv', stringsAsFactors = FALSE)
+function(input, output, session) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    
+    Rating <- cocoa[cocoa$Company.Location == input$CompanyLocation,]$Rating
+    bins <- seq(min(cocoa$Rating), max(cocoa$Rating), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(Rating, breaks = bins, col = '#6d5947', border = 'white')
+    
+  })
+}
