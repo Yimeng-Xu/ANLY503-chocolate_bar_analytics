b4ae0e1e9168bbb1add05a61b32ef93644d3438e
Yimeng-Xu <35519677+Yimeng-Xu@users.noreply.github.com>
2018-12-18 15:29:10 +0800

    ShinyApp

---
+++ ShinyApp/ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+cocoa <- read.csv('data/flavors_of_cocao_clean-united states.csv', stringsAsFactors = FALSE)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  titlePanel("Dark Chocolate Rating Distribution by Company Location"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "bins",
+        "Number of bins:",
+        min = 1,
+        max = 50,
+        value = 30
+      ),
+      selectInput("CompanyLocation", "Company Location:", choices = cocoa$Company.Location),
+      hr(),
+      helpText("Dark Chocolate Bar Data From flavorsofcocao.com")
+    ),
+    mainPanel(
+      plotOutput("distPlot"),
+      plotOutput("HPlot")
+    )
+  )
+  
+)
+)
